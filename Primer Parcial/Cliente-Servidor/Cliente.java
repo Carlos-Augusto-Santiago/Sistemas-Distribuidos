a34b1365eb008b6577e8bae33d731bb8f5cf5b63
Carlos Santiago <nwasantiago611@gmail.com>
2022-08-27 20:13:17 -0500

    Cliente con reintentos y envio de double con writeDouble y ByteBuffer

--- Primer Parcial/Cliente-Servidor/Cliente.java
+++ Primer Parcial/Cliente-Servidor/Cliente.java
@@ -4,38 +4,63 @@ import java.net.Socket;
 import java.nio.ByteBuffer;
 
 public class Cliente {
-    public static void main (String [] args)
-    {
-        try{
-            Socket conexion = new Socket("localhost",50000);
-            DataOutputStream salida = new DataOutputStream(conexion.getOutputStream());
-            DataInputStream entrada = new DataInputStream(conexion.getInputStream());
-            salida.writeInt(123);
-            salida.writeDouble(1234567890.1234567890);
-            salida.write("hola".getBytes());
+    public static void main(String[] args) {
+        Socket conexion = null;
+        for (;;)
+            try {
+                conexion = new Socket("localhost", 50000);
+                DataOutputStream salida = new DataOutputStream(conexion.getOutputStream());
+                DataInputStream entrada = new DataInputStream(conexion.getInputStream());
+                salida.writeInt(123);
+                salida.writeDouble(1234567890.1234567890);
+                salida.write("hola".getBytes());
 
-            byte[] buffer = new byte[4];
-            read(entrada,buffer,0,4);
-            System.out.println(new String(buffer,"UTF-8"));
+                // Tiempo de inicio de enviar los 10000 numeros
+                long start = System.currentTimeMillis();
+                // Enviar 10000 numeros con write Double
+                for (double i = 1.0; i <= 10000.0; i += 1.0) {
+                    salida.writeDouble(i);
+                }
+                // Tiempo de finalizacion del proceso
+                long end = System.currentTimeMillis() - start;
 
-            ByteBuffer b = ByteBuffer.allocate(5*8);
-            b.putDouble(1.1);
-            b.putDouble(1.2);
-            b.putDouble(1.3);
-            b.putDouble(1.4);
-            b.putDouble(1.5);
-            byte[] a = b.array();
-            salida.write(a);
-            Thread.sleep(1000);
-            conexion.close();
-        }
-        catch(Exception e){}
+                System.out.println("El tiempo en s fue de: " + end + "ms");
+                byte[] buffer = new byte[4];
+                read(entrada, buffer, 0, 4);
+                System.out.println(new String(buffer, "UTF-8"));
+
+                ByteBuffer b = ByteBuffer.allocate(5 * 8);
+                b.putDouble(1.1);
+                b.putDouble(1.2);
+                b.putDouble(1.3);
+                b.putDouble(1.4);
+                b.putDouble(1.5);
+                byte[] a = b.array();
+                salida.write(a);
+
+                // Tiempo de inicio de enviar los 10000 numeros
+                long startB = System.currentTimeMillis();
+                // Envio de 10000 double con ByteBuffer
+                ByteBuffer nums = ByteBuffer.allocate(10000 * 8);
+                for (double i = 1.0; i < 10000.0; i += 1.0) {
+                    nums.putDouble(i);
+                }
+                byte[] numbers = nums.array();
+                salida.write(numbers);
+                // Tiempo de finalizacion del proceso
+                long endB = System.currentTimeMillis() - startB;
+                System.out.println("El tiempo en s fue de: " + endB + "ms");
+
+                Thread.sleep(1000);
+                conexion.close();
+                break;
+            } catch (Exception e) {
+            }
     }
-    static void read(DataInputStream f,byte[] b,int posicion,int longitud) throws Exception
-    {
-        while (longitud > 0)
-        {
-            int n = f.read(b,posicion,longitud);
+
+    static void read(DataInputStream f, byte[] b, int posicion, int longitud) throws Exception {
+        while (longitud > 0) {
+            int n = f.read(b, posicion, longitud);
             posicion += n;
             longitud -= n;
         }
--- Primer Parcial/Cliente-Servidor/Servidor.java
+++ Primer Parcial/Cliente-Servidor/Servidor.java
@@ -6,9 +6,8 @@ import java.nio.ByteBuffer;
 
 public class Servidor {
 
-    public static void main (String [] args)
-    {
-        try{
+    public static void main(String[] args) {
+        try {
             ServerSocket servidor = new ServerSocket(50000);
             Socket conexion = servidor.accept();
 
@@ -18,27 +17,52 @@ public class Servidor {
             int n = entrada.readInt();
             System.out.println(n);
 
+            double x = entrada.readDouble();
+            System.out.println(x);
+
             byte[] buffer = new byte[4];
-            read(entrada,buffer,0,4);
-            System.out.println(new String(buffer,"UTF-8"));
+            read(entrada, buffer, 0, 4);
+            System.out.println(new String(buffer, "UTF-8"));
+
+            // Tiempo de inicio de recibir los 10000 numeros
+            long start = System.currentTimeMillis();
+            // Leer los 10000 numeros double
+            for (int i = 0; i < 10000; i++) {
+                double y = entrada.readDouble();
+            }
+            // Tiempo de finalizacion del proceso
+            long end = System.currentTimeMillis() - start;
+
+            System.out.println("El tiempo en s fue de: " + end + "ms");
+
             salida.write("HOLA".getBytes());
 
-            byte[] a = new byte[5*8];
-            read(entrada,a,0,5*8);
+            byte[] a = new byte[5 * 8];
+            read(entrada, a, 0, 5 * 8);
             ByteBuffer b = ByteBuffer.wrap(a);
-            for (int i = 0; i < 5; i++) System.out.println(b.getDouble());
+            for (int i = 0; i < 5; i++)
+                System.out.println(b.getDouble());
+
+            // Tiempo de inicio de enviar los 10000 numeros
+            long startB = System.currentTimeMillis();
+
+            byte[] numbers = new byte[10000 * 8];
+            read(entrada, numbers, 0, 10000 * 8);
+            ByteBuffer nums = ByteBuffer.wrap(numbers);
+
+            // Tiempo de finalizacion del proceso
+            long endB = System.currentTimeMillis() - startB;
+            System.out.println("El tiempo en s fue de: " + endB + "ms");
 
             conexion.close();
-        }
-        catch(Exception e){
+        } catch (Exception e) {
 
         }
     }
-    static void read(DataInputStream f,byte[] b,int posicion,int longitud) throws Exception
-    {
-        while (longitud > 0)
-        {
-            int n = f.read(b,posicion,longitud);
+
+    static void read(DataInputStream f, byte[] b, int posicion, int longitud) throws Exception {
+        while (longitud > 0) {
+            int n = f.read(b, posicion, longitud);
             posicion += n;
             longitud -= n;
         }
