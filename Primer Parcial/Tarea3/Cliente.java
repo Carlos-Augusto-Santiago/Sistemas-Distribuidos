94492a8e1219290e5813819a0ad20ef2048f0ce8
Carlos Santiago <carlos@thedisruptoragency.com>
2022-10-24 11:43:49 -0500

    ok

--- Primer Parcial/Tarea3/Cliente.java
+++ Primer Parcial/Tarea3/Cliente.java
@@ -1,20 +1,21 @@
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
+import java.net.UnknownHostException;
+import java.nio.DoubleBuffer;
+import java.nio.FloatBuffer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Programa {
+public class Cliente {
 
-    static int N = 4;
+    static int N = 2;
     static float[][] A = new float[N][N];
     static float[][] B = new float[N][N];
-    static float[][] C = new float[N][N];
+    static boolean reintentar = true;
 
     // arreglos de A1-A4
     static float[][] A1 = new float[N][N];
@@ -31,8 +32,10 @@ public class Programa {
     // variable para la copia de la matriz B
     static float[][] B_copy = new float[N][N];
 
+    // variable para los valores de C1-C16
+    static float[] C_values = new float[16];
     // puerto inicial para los hilos
-    static int PORT_START = 50000;
+    static int PORT_START = 21864;
 
     public static void Matrices() {
         // inicializa las matrices A y B
@@ -64,7 +67,6 @@ public class Programa {
                 B[i][j] = 2 * i - j;
                 // copia de la matriz B
                 B_copy[i][j] = B[i][j];
-                C[i][j] = 0;
             }
             System.out.println("");
             if (i == N - 1) {
@@ -116,75 +118,109 @@ public class Programa {
                 }
             }
         }
-
-        // multiplica la matriz A y la matriz B, el resultado queda en la matriz C
-        // notar que los indices de la matriz B se han intercambiado
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                for (int k = 0; k < N; k++) {
-                    C[i][j] = A[i][k] * B[j][k];
-                }
-            }
-        }
     }
 
     static class Worker extends Thread {
 
-        int port;
+        Socket socket;
+        int num;
 
-        Worker(int port) {
-            this.port = port;
+        Worker(Socket socket, int num) {
+            this.socket = socket;
+            this.num = num;
         }
 
         public void run() {
             try {
                 // iniciamos el hilo en el puerto asignado
-                ServerSocket server = new ServerSocket(port);
-                Socket conexion = server.accept();
-                PrintWriter salida = new PrintWriter(conexion.getOutputStream());
-                BufferedReader entrada = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
+                DataOutputStream salida = new DataOutputStream(socket.getOutputStream());
+                BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                // enviar las matrices B1-B4
+                FloatBuffer buffer = FloatBuffer.allocate(N);
+                for (int i = 0; i < B1.length; i++) {
+                    buffer.put(B1[i]);
+                }
+                float[][] Bs = buffer.array();
+
+                // se checa en que hilo esta, si es el 1 manda A1 y A2
+                // si es el dos manda A3 y A4
+                // if (num == 1) {
+                // System.out.println("Enviando A1, A2 y B1-B4 al hilo 1");
+                // salida.println(A1);
+                // salida.println(A2);
+                // salida.flush();
+
+                // // recibir las matrices C1-C8
+                // for (int i = 0; i < 8; i++) {
+                // C_values[i] = Float.parseFloat(entrada.readLine());
+                // }
+                // } else if (num == 2) {
+                // System.out.println("Enviando A3, A4 y B1-B4 al hilo 2");
+                // salida.println(A3);
+                // salida.println(A4);
+                // salida.flush();
+                // // recibir las matrices C9-C16
+                // for (int i = 8; i < 16; i++) {
+                // C_values[i] = Float.parseFloat(entrada.readLine());
+                // }
+                // }
+
+                reintentar = false;
 
             } catch (Exception e) {
-                // TODO: handle exception
             }
         }
     }
 
     public static void main(String[] args) throws Exception {
-        // inicializamos el servidor
-        ServerSocket servidor = new ServerSocket(50000);
-        // declarar la clase worker en w
-        Worker w;
-        // lista de workers para usar multi hilos
-        List<Worker> fileWorkers = new ArrayList<Worker>();
+        Socket conexion = null;
         // asignacion de las matrices usado la funcion Matrices
         Matrices();
         // ciclo de reconexiones
-        while (true) {
-            // iniciar conexion
-            Socket conexion = servidor.accept();
-            System.out.println("Conexion recibida");
+        while (reintentar) {
+            Worker[] hilos;
             try {
-                PrintWriter salida = new PrintWriter(conexion.getOutputStream());
-                System.out.println("Inicializando los 4 hilos de las computadoras");
-                for (int i = 0; i < 4; i++) {
-                    System.out.println("Iniciando hilo con puerto: " + (PORT_START + i));
-                    w = new Worker(PORT_START + i);
-                    w.start();
-                    fileWorkers.add(w);
-                }
-                salida.println("Ya quedó c:");
-                salida.flush();
-                System.out.println("Conectado!");
+                conexion = new Socket("localhost", 8080);
+                // iniciar conexion
+                System.out.println("Conexion recibida");
+                BufferedReader entrada = new BufferedReader(
+                        new InputStreamReader(conexion.getInputStream()));
 
-                System.out.println("Esperando a que se terminen de ejecutar los hilos...");
-                for (int i = 0; i < fileWorkers.size(); ++i) {
-                    fileWorkers.get(i).join();
+                // asegurando que se abrieron los sockets
+                System.out.println("Esperando la confirmacion del servidor...");
+                entrada.readLine();
+                System.out.println("Confirmacion completada");
+
+                // Enviar las matrices e iniciar su Hilo
+                System.out.println("Iniciando hilos para lectura de las matrices");
+                hilos = new Worker[2];
+                for (int i = 0; i < 2; i++) {
+                    try {
+                        Worker w = new Worker(new Socket("localhost", PORT_START + i), i + 1);
+                        w.start();
+                        hilos[i] = w;
+                    } catch (UnknownHostException ex) {
+                        Logger
+                                .getLogger(Cliente.class.getName())
+                                .log(Level.SEVERE, null, ex);
+                    }
                 }
                 System.out.println("Listo");
-            } catch (IOException e) {
-                Logger.getLogger(Programa.class.getName()).log(Level.SEVERE, null, e);
+
+                System.out.println("Esperando a que terminen de enviarse los archivos");
+                // Cerramos los hilos
+                for (int i = 0; i < 2; i++) {
+                    hilos[i].join();
+                }
+
+                // Cerrar el socket
+                conexion.close();
+                System.out.println("Proceso terminado");
+            } catch (InterruptedException | IOException ex) {
+                Logger
+                        .getLogger(Cliente.class.getName())
+                        .log(Level.SEVERE, null, ex);
             }
         }
     }
---
+++ Primer Parcial/Tarea3/Servidor.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Servidor {
+
+    static int PORT_START = 21864;
+
+    static class Worker extends Thread {
+        ServerSocket server;
+
+        Worker(ServerSocket server) {
+            this.server = server;
+        }
+
+        public void run() {
+            try {
+                Socket conexion = server.accept();
+
+                PrintWriter salida = new PrintWriter(conexion.getOutputStream());
+                BufferedReader entrada = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
+
+                // recibir las matrices B
+                List<float[][]> B = new ArrayList<float[][]>();
+                for (int i = 0; i < 6; i++) {
+                    float value = Float.valueOf(entrada.readLine()).floatValue();
+                    System.out.println("Valor: " + value);
+                }
+                salida.flush();
+                salida.close();
+                entrada.close();
+                conexion.close();
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Worker w;
+        List<Worker> Workers = new ArrayList<Worker>();
+        ServerSocket server = new ServerSocket(8080);
+        while (true) {
+            Socket conexion = server.accept();
+            try {
+                PrintWriter salida = new PrintWriter(conexion.getOutputStream());
+                BufferedReader entrada = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
+                // iniciar los hilos con el puerto correspondiente
+                for (int i = 0; i < 2; i++) {
+                    System.out.println("Iniciando hilo en el puerto: " + (PORT_START + i));
+                    w = new Worker(new ServerSocket(PORT_START + i));
+                    w.start();
+                    Workers.add(w);
+                }
+                salida.println("Ya quedó c:");
+                salida.flush();
+                System.out.println("Conectado!");
+
+                System.out.println("Esperando a que se terminen de recibir las matrices...");
+                for (int i = 0; i < Workers.size(); ++i) {
+                    Workers.get(i).join();
+                }
+                System.out.println("Listo");
+
+                salida.close();
+                entrada.close();
+                conexion.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+    }
+}
